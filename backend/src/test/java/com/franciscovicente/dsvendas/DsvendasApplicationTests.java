6e20599e0da6b72b02c6af787de1ed322eb9b78d
Francisco Vicente A. Neto <vicenteneto.ti@gmail.com>
2021-05-11 23:38:41 +0100

    Created Projects

---
+++ backend/src/test/java/com/franciscovicente/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscovicente.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
