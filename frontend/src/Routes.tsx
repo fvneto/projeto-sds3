a0ab6ab8d3bd19edaf86af8f7729db8686554119
Francisco Vicente A. Neto <vicenteneto.ti@gmail.com>
2021-05-19 10:28:52 +0100

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -9,7 +9,7 @@ const Routes = () => {
             <Route path="/" exact>
                 <Home/>
             </Route>
-            <Route path="/dasboard">
+            <Route path="/dashboard">
                 <Dashboard/>
             </Route>
         </Switch>
